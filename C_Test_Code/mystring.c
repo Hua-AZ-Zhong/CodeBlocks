0fca268420d0f8f4b8577083e295270b1232cbda
Hua-AZ-Zhong <253729119@qq.com>
2018-08-19 17:35:42 +0800

    string_rewrite

--- C_Test_Code/mystring.c
+++ C_Test_Code/mystring.c
@@ -22,7 +22,7 @@ void my_strcpy(char *dest, char *src)
 
 void reverse(char *src)
 {
-    char tmp[1024];
+    char tmp[1024]={0};
     char *p=tmp;
     puts(tmp);
     int num=0;
@@ -34,26 +34,51 @@ void reverse(char *src)
 
     for(int i=0;i<num;i++)
     {
-        *p++ = *(--src);
-        printf("p is [%c], src is [%c], tmp[%d]=[%c]\n", *p, *src, i, tmp[i]);
+        *p++=*(--src);
     }
 
     my_strcpy(src,tmp);
 }
+
+void reverse_new(char *src, char *dest)
+{
+    int num=0;
+    while(*src!='\0')
+    {
+        src++;
+        num++;
+    }
+
+    for(int i=0;i<num;i++)
+    {
+        --src;
+        *dest = *src;
+        printf("dest is [%c], src is [%c]\n", *dest, *src);
+        dest++;
+    }
+}
+
 int main()
 {
     //char *string_A="hello";   //此种方式定义的字符串位于静态存储区，不允许通过指针修改内容
     char string_A[100]={"hello"};
-    char *string_C="world!";
+    char *string_C="_world!";
     printf("strlen of A is [%d] cont is [%s]\n",strlen(string_A),string_A);
     my_strcat(string_A,string_C);
     printf("strlen of A is [%d] cont is [%s]\n",strlen(string_A),string_A);
     puts(string_A);
+
     memset(string_A,0x00,100);
     puts(string_A);
     my_strcpy(string_A,string_C);
     puts(string_A);
+
     reverse(string_A);
     puts(string_A);
+
+    memset(string_A,0x00,100);
+    reverse_new(string_C, string_A);
+    puts(string_A);
+
     return 0;
 }
