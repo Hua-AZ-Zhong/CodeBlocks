3f3e1ea64af08ee392cc673f75d70023c0105641
zhonghua <253729119@qq.com>
2018-10-27 22:27:58 +0800

    update

--- Java/TestDemo.java
+++ Java/TestDemo.java
@@ -1,12 +1,15 @@
 class Person{
 	String name;
 	int age;
+	public void display(){
+		System.out.println("Name = " + name + " , Age = " + age);
+	}
 }
 
 public class TestDemo{
 	public static void main(String args[]){
 		Person per = null;
 		per = new Person();
-		System.out.println("Name = " + per.name + "," + "Age = " + per.age);
+		per.display();
 	}
 }
