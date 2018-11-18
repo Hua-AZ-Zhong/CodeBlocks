6b5fbcd1707fa54f9553d507339ba34c0f25fd0c
zhonghua <253729119@qq.com>
2018-11-18 18:10:43 +0800

    update

--- UnixC/day01/usewelcome.c
+++ UnixC/day01/usewelcome.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 int main(int argc, char* argv[])
 {
-	if(argc!=2){printf("usage:%s name\n",argv[0]);}
-	else welcome(argv[1]);
+	if(argc!=2)
+		printf("usage:%s name\n",argv[0]);
+	else
+		welcome(argv[1]);
 }
 
