e5675dd6310b1348237799f62ba0a8234147489f
Hua-AZ-Zhong <253729119@qq.com>
2018-11-19 09:34:22 +0800

    Update setitimer.c

--- TestDemo/setitimer.c
+++ TestDemo/setitimer.c
@@ -13,7 +13,10 @@ void signalHandler(int signo)
  
 int main(int argc, char *argv[])
 {
+	#if 0
 	signal(SIGALRM, signalHandler);
+	#endif
+	catch_all_signal();
 	     
         struct itimerval new_value, old_value;
 	new_value.it_value.tv_sec = 0;
@@ -25,4 +28,26 @@ int main(int argc, char *argv[])
 	for(;;);
 					     
 	return 0;
+}
+
+void catch_all_signal()
+{
+
+   signal(SIGINT, signalHandler);  //Control X Signal 2 interrupt, generated from terminal
+   signal(SIGQUIT, signalHandler); //Control D Signal 3 quit, generated from terminal special
+   signal(SIGTSTP, signalHandler); // Signal 18 interactive stop
+   signal(SIGKILL, signalHandler); //Signal 9 kill cannot be trapped, so this line is useless
+   signal(SIGHUP, signalHandler); //Signal 1 - Hangup
+   signal(SIGCHLD, signalHandler); //Signal 20 sent to parent on child stop or exit
+   signal(SIGIO, signalHandler); // Signal 23 - I/O possible, or completed
+   signal(SIGSEGV, signalHandler); // Signal 11 - Core Dump
+   signal(SIGTERM, signalHandler); // Signal 15 - Software terminate
+
+   #if 0
+   for(int i=1; i <= 49 ; i++)
+   {   
+	signal(i,signalHandler);     
+   }
+   #endif
+
 }
