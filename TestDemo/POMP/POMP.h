7c854c5fcb99aea7e72ba8de77571a253aa121ff
Hua-AZ-Zhong <253729119@qq.com>
2019-01-11 10:40:35 +0800

    Create POMP.h

---
+++ TestDemo/POMP/POMP.h
@@ -0,0 +1,16 @@
+struct POMP {
+    int TranId;
+    char TerId[9];
+    char CardId[20];
+    char ProName[11];
+    char ModName[21];
+    char NodeId[3];
+    char TxnID[3];
+    char TimeType[3];
+    char TxnDateTime[24];
+    char LinkSysName[21];
+    char ReturnCode[11];
+    char ReturnMsg[1001];
+    char SucFlag[2];
+    char costtime[7];
+}
