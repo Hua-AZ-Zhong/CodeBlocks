76b057f4f04404edbb9debc9298a09cc1f7828d5
Hua Zhong <zhonghua@asuslaptop03.desktop.ubuntu.vmware>
2021-04-25 15:16:40 +0800

    cx_Oracle testDemo

---
+++ Python/cx_Oracle/query.py
@@ -0,0 +1,25 @@
+# query.py
+
+import cx_Oracle
+
+userpwd = "BOCSWT"
+# Establish the database connection
+#connection = cx_Oracle.connect("hr", userpwd, "dbhost.example.com/orclpdb1")
+connection = cx_Oracle.connect("bocswt", userpwd, "192.168.6.248/XE")
+
+# Obtain a cursor
+cursor = connection.cursor()
+
+# Data for binding
+instId = "3000"
+userId = "      3000"
+
+# Execute the query
+sql = """SELECT *
+         FROM shcbin
+         WHERE institutionid = :p1 AND userbinid = :p2"""
+cursor.execute(sql, p1 = instId, p2 = userId)
+
+# Loop over the result set
+for row in cursor:
+    print(row)
